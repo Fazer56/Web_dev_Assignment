af4d2fb3efadba35e7207ad52b93bf096543a4b4
Fazer56 <faoilean56@gmail.com>
2017-11-13 11:31:34 +0000

    Added Database file

--- WebApp/Index2.php
+++ WebApp/Index2.php
@@ -1,3 +1,6 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE Html>
 <html>
 	<head>
@@ -45,64 +48,51 @@
 
               <label style="margin-top: 10px;color:white;">Search </label> 
               <input type="search" style="margin-top: 10px;" name="search" id="searchBox">
-              <input style="margin-top: 10px; float:right; background: #2B2B2B; color: #0167BB;" type="button" id="btn1" href="login.php" value="log-in">
-
+              <a href="login.php"><input style="margin-top: 10px; float:right; background: #2B2B2B; color: #0167BB;" type="button" id="btn1" value="log-in"></a>
+              
             </div>
           </div>
         </nav>
 
       </div>
     </div>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-4"></div>
-        <div class="col-sm-4"></div>
-        <div class="col-sm-4"></div>
-      </div>
-      <div class="row">
-          <div class="col-sm-4"></div>
-          <div class="col-sm-4">
-            <h1>Login</h1>
-            <form id="form1">
-            <div class="form-group">
-                <label for="username" >Username: </label>
-                <input type="text" class="form-control" id="username1" pattern="[A-Za-z]+[0-9]+" required>
-                <br/>
-                <input type="submit" class="btn btn-primary" value="submit" id="submit1"> 
-            </div>
-            </form>
-          </div>
-      </div>    
-      <div class="row">
-          <div class="col-sm-4"></div>
-          <div class="col-sm-4">
-            <h1>Register</h1>
-            <form id="form2" method=\"post\" action=\"register.php\">
-              <div class="form-group">
-              <label for="username" >Username: </label>
-                <input type="text" class="form-control" id="username" name="username" pattern="[A-Za-z0-9]+" required>
-                
+     <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  		<!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		    <li data-target="#myCarousel" data-slide-to="1"></li>
+		    <li data-target="#myCarousel" data-slide-to="2"></li>
+		  </ol>
 
-                <label for="firstName" >First Name: </label>
-                <input type="text" class="form-control" id="firstName" pattern="[A-Za-z]+" required>
+  <!-- Wrapper for slides -->
+  		<div class="carousel-inner">
+    		<div class="item active">
+      			<img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" alt="Los Angeles">
+    		</div>
 
-                <label for="lastName">Last Name: </label>
-                <input type="text" class="form-control" id="lastName" pattern="[A-Za-z]+" required>
+		    <div class="item">
+		      <img src="http://www.jbsuits.com/blog/wp-content/uploads/2017/03/deadpool-ryan-ronalds.jpg" alt="Chicago">
+		    </div>
 
-                <label for="email" >Email: </label>
-                <input type="email" class="form-control" id="email" pattern="[\w\-\.\+]+\@[A-Za-z0-9\.\-]+\.[a-zA-Z0-9]{2,4}" required>
+		    <div class="item">
+		      <img src="https://ewedit.files.wordpress.com/2017/06/dunkirk_sdtk_cover_01_1425px_rgb_300dpi.jpg?w=2400&h=2400" alt="New York">
+		    </div>
+  		</div>
 
-                <br/>
+  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+</div>
 
-                <input type="submit" class="btn btn-primary" value="submit" id="submit1"> 
+	<script>
 
-              </div>
-              </form>
-          </div>
-      </div>
-    </div>
-  </div>
-     
+	</script>
 
 	</body>
 </html>
---
+++ WebApp/register.php
@@ -0,0 +1,100 @@
+	
+	
+<?php
+
+	$usernameErr = $firstnameErr = $lastnameErr = $emailErr = $genderErr = $websiteErr = "";
+	$username1 = $firstname = $lastname = $email = $gender = $comment = $website = "";
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["username"])) {
+    $usernameErr = "User Name is required";
+  } else {
+    $usernameErr = test_input($_POST["username"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z0-9]+$/",$username1)) {
+      $usernameErr = "Only letters and numbers no white space allowed"; 
+    }
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+
+  if (empty($_POST["firstName"])) {
+    $firstnameErr = "First name is required";
+  } else {
+    $firstname = test_input($_POST["firstName"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/^[A-Za-z]+$/",$firstname)) {
+      $firstnameErr = "Only letters and no whitespace aloud"; 
+    }
+  }
+
+  if (empty($_POST["lastName"])) {
+    $lastnameErr = "Last name is required";
+  } else {
+    $lastname = test_input($_POST["lastName"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/^[A-Za-z]+$/",$lastname)) {
+      $lastnameErr = "Only letters and no whitespace aloud"; 
+    }
+  }
+
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+
+}	
+
+
+	$servername="localhost";
+	$username="root";
+	$password="faoilean56";
+	$dbname="mydb";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // prepare sql and bind parameters
+   $stmt =$conn->prepare("INSERT INTO user(userName,firstName,lastName, email) 
+		VALUES(:username, :firstname, :lastname, :email)");
+
+	$stmt->bindParam(':username', $username);
+	$stmt->bindParam(':firstname', $firstname);
+	$stmt->bindParam(':lastname', $lastname);
+	$stmt->bindParam(':email', $email);
+
+    // insert a row
+    $username = $_POST["username"];
+    $firstname = $_POST["firstName"];
+    $lastname = $_POST["lastName"];
+    $email = $_POST["email"];
+    $stmt->execute();
+
+     $url='http://localhost/WebApp/Index2.php';
+
+    echo '<script>window.location = "'.$url.'";</script>';
+    die;
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+$conn = null;
+
+
+
+?>
