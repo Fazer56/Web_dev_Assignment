3013aba5f0943c1fcb3c876942dd52a6c58b2c02
c15492952 <alexwindows8music@gmail.com>
2017-11-16 21:06:31 +0000

    movies almost finished

--- WebApp/movies.php
+++ WebApp/movies.php
@@ -29,6 +29,19 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  <link rel="stylesheet" href="jquery-ui.css">
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+function swapContent(cv){
+	var url = "myphpscript.php";
+	$.post(url, {contentVar:cv}, function(data){
+		$("#myDiv").html(data).show();
+	});
+}
+
+</script>
 	</head>
 	<body>
     <div class="navbar-wrapper">
@@ -43,12 +56,12 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="Index2.php">Movies AF</a>
+              <a class="navbar-brand" href="Index.php">Movies AF</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav" >
-                <li ><a href="Index2.php">Home</a></li>
-                <li class="active"><a href="/aboutapp">Movies</a></li>
+                <li ><a href="Index.php">Home</a></li>
+                <li class="active"><a href="movies.php">Movies</a></li>
                 <li><a href="/contact">Watchlist</a></li>     
                 <li> </li>
                 <li> </li>
@@ -77,23 +90,23 @@
             ?>
 
     </div>
-     <div class="container-fluid" style=" margin: auto; width:50%; background-color: #fcfcff;margin-top: -2%;">
-     
+     <div class="container-fluid" style=" margin-left: 15%; width:50%; background-color: #fcfcff;margin-top: -2%;">
+	
+	</br></br></br>
      
       <div class="row">
         <div class="col-sm-4"></div>
         <div class="col-sm-4"></div>
         <div class="col-sm-4"></div>
       </div>
-
-      <div class="row" >
+	  <center>
         <?php
 
         $id = isset($_GET['id']) ? $_GET['id'] : '';
 
-          $servername="localhost";
-          $username="root";
-          $password="faoilean56";
+          $servername="147.252.138.146";
+          $username="root2";
+          $password="alex";
           $dbname="database";
 
           $conn = new mysqli($servername, $username, $password, $dbname);
@@ -103,191 +116,50 @@
           } 
 
 
-          $sql = "SELECT movie_id, movie_name, director, movie_img, rating FROM movies";
+          $sql = "SELECT movie_name, movie_id, rating, movie_img FROM movies";
 
           $result = $conn->query($sql);
+		  
+		 
 
-          //if($result) //check query worked
-          //{
+          if($result) //check query worked
+          {
 
-           /* echo '<div class="row"  >
-                      <div class="col-sm-1"> 
-                      </div>
-                      <div class="col-sm-3"></div>
-                     <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-                  </div>';*/
-
-
-                         //get each row in the table
-              while($row = mysqli_fetch_array($result)){
+			  
+			   while($row = mysqli_fetch_array($result)){
+				   $id = $row['movie_id'];
                   echo '<div class="row" style="padding-top: 5%;">
-                          <div class="col-sm-1"> 
-                            <input type="image" src="'.$row['movie_img'].'" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;"> 
-                          </div>
-                           <div class="col-sm-3"></div>
-                          <div class="col-sm-6"> <h3>'.$row['movie_id'].'. '.$row['movie_name'].'</h3> <p>Rating '.$row['rating'].'</p> <br/></div>
-                    </div>';
-              
-              }
-          //}  
+								<div class ="col-sm-1">
+								<a href="#" onClick="return false" onmousedown="javascript:swapContent('.$row['movie_id'].')">
+								<input type="image" name="'.$row['movie_id'].'" src="'.$row['movie_img'].'" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;"> </a>
+								  </div>
+								   <div class="col-sm-3"></div>
+								  <div class="col-sm-6"> <legend><h3>'.$row['movie_id'].'. '.$row['movie_name'].'</h3> </legend><p>Rating '.$row['rating'].'</p><br/> <input type="button" action="add.php" method="POST" value="Add To Watchlist" name="'.$_SESSION['id'].'"> <br/></div>
+							</div>';
+						  
+			   }
+          }  
 
           $conn = null;
-
-        ?>
-
+?>
     
 
        <br/><br/><br/>
 
-      <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
        <br/><br/><br/>
 
-
-      <!-- <div class="row"  >
      
-        <div class="col-sm-1"> 
-              <input src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div>
-
-       <br/><br/><br/>
-
-
-             <div class="row"  >
-     
-        <div class="col-sm-1"> 
-              <img src="http://static.tumblr.com/8bfc962a5ca8f773d634b9aec670e029/yxql1hy/tjtov7rlt/tumblr_static_26osk19e9qck84sgcwogsw88w.jpg" style="margin-left: 60%;margin-top: 60%; height: 700%; width:700%;">
-          </div>
-          <div class="col-sm-3"></div>
-        <div class="col-sm-6"> <h3> 1. Blade Runner </h3> <p>Rating 8.3</p> <br/><p>A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years. </p></div>
-        
-      </div> -->
-
-       <br/><br/><br/>
-        
       
 
     </div>
-     
-    </div>
-  
-</div>
+     <div id="myDiv">
+		<div class="container" style="width: 30%; height: 30%; position: fixed; left: 950px; top: 100px">
+		  <div class="jumbotron">
+			<legend><Center><h3>Select a Movie</h3></center></legend>
+			<img src="https://www.kolayuyelik.com/wp-content/uploads/2015/01/Digiturk-film.png"  width="330" height="330">
+		  </div>    
+		</div>
+	</div>
 
 	<script>
 
